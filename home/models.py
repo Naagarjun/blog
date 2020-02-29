ff209f740d844d249731341ada8fd195294cb89c
Naagarjun <nagarjuna0402@gmail.com>
2020-02-29 14:49:14 +0530

    Updated modified  models,urls,views and  home.hmtl

--- home/models.py
+++ home/models.py
@@ -2,11 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-from django.utils.text import slugify
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=255)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -15,13 +14,4 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        kwarg= {
-            'pk': self.pk,
-            'slug': self.slug
-        }
-        return reverse('post-detail', kwargs=kwarg)
-
-    def save(self, *args, **kwargs):
-        value = self.title
-        self.slug = slugify(value, allow_unicode=True)
-        super().save(*args, **kwargs)
+        return reverse('post-detail', kwargs={'pk': self.pk})
