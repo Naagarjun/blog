499f5f6f80b2e2a5436f8e99426b4ae020d2dc81
Naagarjun <nagarjuna0402@gmail.com>
2020-03-05 15:32:40 +0530

    Added image croping

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
-from django.core.files.storage import default_storage as storage
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -10,16 +10,12 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} profile'
 
-    def save(self):
-        if not self.id and not self.project_description:
-            return
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
 
-        super(Projects, self).save()
-        if self.project_thumbnail:
-            size = 200, 200
-            image = Image.open(self.project_thumbnail)
-            image.thumbnail(size, Image.ANTIALIAS)
-            fh = storage.open(self.project_thumbnail.name, "w")
-            format = 'png'  # You need to set the correct image format here
-            image.save(fh, format)
-            fh.close()
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)'
