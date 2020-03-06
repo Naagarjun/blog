87b6cc4d2d9dd280631fb9fee720245a61eb795f
Naagarjun <nagarjuna0402@gmail.com>
2020-03-06 17:02:04 +0530

    Modified save method img.self.file from path in models

--- accounts/models.py
+++ accounts/models.py
@@ -16,6 +16,6 @@ class Profile(models.Model):
         img = Image.open(self.image)
 
         if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
+            output_size = (100, 100)
             img.thumbnail(output_size)
             img.save(self.image.file)
