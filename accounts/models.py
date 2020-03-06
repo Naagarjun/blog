7b1b5bca71310a128d9df10eb20643e276d5a956
Naagarjun <nagarjuna0402@gmail.com>
2020-03-06 21:56:40 +0530

    Modified and returned to models default

--- accounts/models.py
+++ accounts/models.py
@@ -11,9 +11,9 @@ class Profile(models.Model):
         return f'{self.user.username} profile'
 
     def save(self, *args, **kwargs):
-        super(Profile, self).save(*args, **kwargs)
-        img = Image.open(self.image)
+        super().save(*args, **kwargs)
+        img = Image.open(self.image.path)
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
-            img.save(self.image.file)
+            img.save(self.image.path)
