1a864dc370a7d023b1e37fec49f5211c603c30c0
Naagarjun <nagarjuna0402@gmail.com>
2020-03-08 11:45:43 +0530

    Modified models save method by django default storage

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
-# from io import BytesIO
-# from django.core.files.storage import default_storage as storage
+from io import BytesIO
+from django.core.files.storage import default_storage as storage
 
 
 class Profile(models.Model):
@@ -13,12 +13,15 @@ class Profile(models.Model):
         return f'{self.user.username} profile'
 
     def save(self, *args, **kwargs):
-
-        super(Profile, self).save(*args, **kwargs)
+        super().save(*args, **kwargs)
+        mem_file = BytesIO()
 
         img = Image.open(self.image)
 
         if img.height > 400 or img.width > 400:
-            output_size = (430,360)
-            img.thumbnail(output_size)
-            img.save(self.image)
+            output_size = (400, 400)
+            img.thumbnail(output_size, Image.ANTIALIAS)
+            img.save(mem_file, 'JPEG', quality=80)
+            storage.save(self.image.name, mem_file)
+            mem_file.close()
+            img.close()
