329a21641bb95fa5bb1cab01bc82147745006219
Naagarjun <nagarjuna0402@gmail.com>
2020-03-14 14:07:53 +0530

    Changed Model date_posted to publish

--- home/views.py
+++ home/views.py
@@ -15,7 +15,7 @@ class PostListView(ListView):
     model = Post
     template_name = 'home/home.html'
     context_object_name = 'posts'
-    ordering = ['-date_posted']
+    ordering = ['-publish']
     paginate_by = 2
 
 
@@ -27,7 +27,7 @@ class UserPostListView(ListView):
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
-        return Post.objects.filter(author=user).order_by('-date_posted')
+        return Post.objects.filter(author=user).order_by('-publish')
 
 
 class PostDetailView(DetailView):
