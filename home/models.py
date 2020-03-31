a965e91f4d61a18f4084d5c8498916d0133eaf54
Naagarjun <nagarjuna0402@gmail.com>
2020-03-31 22:10:47 +0530

    Added sorl thumbnail

--- home/models.py
+++ home/models.py
@@ -3,15 +3,13 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils.text import slugify
-from django_comments.moderation import CommentModerator
-from django_comments_xtd.moderation import moderator, SpamModerator
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(default='', max_length=255)
     content = models.TextField()
-    publish = models.DateTimeField(default=timezone.now)
+    date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -27,10 +25,3 @@ class Post(models.Model):
         value = self.title
         self.slug = slugify(value, allow_unicode = True)
         super().save(*args, **kwargs)
-
-class PostCommentModerator(CommentModerator):
-    email_notification = True
-    auto_moderate_field = 'publish'
-    moderate_after = 365
-
-moderator.register(Post, PostCommentModerator)
