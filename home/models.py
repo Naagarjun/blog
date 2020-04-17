73dbda7479da6c23b84b50a9ecf21138f28270fc
Naagarjun <nagarjuna0402@gmail.com>
2020-04-17 08:01:41 +0530

    slug field

--- home/models.py
+++ home/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from django.urls import reverse
-from django.utils.text import slugify
+# from django.urls import reverse
+# from django.utils.text import slugify
 
 
 class Post(models.Model):
@@ -15,11 +15,11 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    def get_absolute_url(self):
-        kwargs={
-                'slug':self.slug 
-                }
-        return reverse('post-detail', kwargs= kwargs)
+    # def get_absolute_url(self):
+    #     kwargs={
+    #             'slug':self.slug 
+    #             }
+    #     return reverse('post-detail', kwargs= kwargs)
 
     # def save(self, *args, **kwargs):
     #     value = self.title
